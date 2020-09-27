ab266976dbb55a900f151c666d434731aaf27606
Steveen <steveen.echeverri@gmail.com>
2020-09-27 17:10:41 -0500

    basic example sub typeof test

---
+++ sub.ts
@@ -0,0 +1,15 @@
+function sub(n1: number, n2: number) {
+    if(typeof n1 !== 'number' || typeof n1 !== 'number') {
+        throw new Error('Incorrect input!');
+    }
+    return n1 - n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = sub(number1, number2);
+
+console.log(result);
+
+// tsc sub.ts
