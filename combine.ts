c64a0f8089f502a9756e93b15d6b093d18b647ee
Steveen <steveen.echeverri@gmail.com>
2020-09-29 16:29:10 -0500

    Change add CombineString new intance

--- combine.ts
+++ combine.ts
@@ -1,17 +1,31 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string
+
+function combine(
+    input1: Combinable, 
+    input2: Combinable,
+    resultConvert: string
+) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
     } else {
         result = input1.toString() + input2.toString;
+    } if(resultConvert === 'as-number') {
+        return +result; // parsfloat(result)
+    } else {
+        return result.toString();
     }
-    return result;
+    
 }
 
-const combineAge = combine(30, 26);
+const combineAge = combine(30, 26, 'as-number');
 console.log(combineAge);
 
-const combineName = combine('Max', 'Lina');
+const combineStringAges = combine('30', '26', 'as-number')
+console.log(combineStringAges);
+
+
+const combineName = combine('Max', 'Lina', 'as-text');
 console.log(combineName);
 
 // tsc combine.ts
