7cbae1428d136c605a22832dcb55851d89ed636b
Steveen <steveen.echeverri@gmail.com>
2020-11-17 12:35:22 -0500

    Completed Project TypeMart

--- Lesson1_Types/TypeMart.ts
+++ Lesson1_Types/TypeMart.ts
@@ -1,6 +1,11 @@
 import products from './products';
 
 const productName: string = 'fanny pack';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress: string = "575 Broadway, New York City, New York "
 
 const product = products.filter(product => {
     return product.name === productName;
@@ -8,6 +13,42 @@ const product = products.filter(product => {
 
 console.log(product);
 
-if(product.preOrder) {
+if (product.preOrder) {
     console.log('We will send you a message when your product ships.');
 }
+
+if (product.price > 25) {
+    console.log('This item will receive free shipping');
+    shipping = 0;
+} else {
+    shipping = 5;
+}
+
+if (shippingAddress.match('New York')) {
+    taxPercent = 0.1;
+} else {
+    taxPercent = 0.05;
+}
+
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
+
+// { name: 'fanny pack', price: 30, preOrder: true }
+// We will send you a message when your product ships.
+// This item will receive free shipping
+
+// Product:  fanny pack
+// Address:  575 Broadway, New York City, New York 
+// Price:    $30
+// Tax:      $3.00
+// Shipping: $0.00
+// Total:    $33.00
