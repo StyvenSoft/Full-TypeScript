d181468b05d4ff9526801d0e5946e9e66dbe6a17
Steveen <steveen.echeverri@gmail.com>
2020-11-19 12:39:05 -0500

    TypeScript’s array types initials

---
+++ Lesson3_Arrays/3_MultidimensionalArrays.ts
@@ -0,0 +1,10 @@
+// Arrays:
+let bestNumbersTwo: number[] = [7, 77, 4];
+let bestLunchesTwo: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfastsTwo: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleansTwo: boolean[] = [true, false];
+
+// Multidimensional Arrays:
+let bestMealPlan: string[][] = [bestLunchesTwo, bestBreakfastsTwo, ['baked potato', 'mashed potato']];
+let bestBooleansTwice: boolean[][] = [bestBooleansTwo, bestBooleansTwo];
+let numbersMulti: number[][][] = [[[1], [2, 3]], [[7], bestNumbersTwo]];
---
+++ Lesson3_Arrays/4_Tuples.ts
@@ -0,0 +1,10 @@
+let tup: [string, string] = ['hi', 'bye'];
+let arr: string[] = ['there','there'];
+tup = ['there', 'there']; // No Errors.
+
+
+let favoriteCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
+
+favoriteCoordinates = [17, 45, 'N', 142, 30, 'E'];
+
+favoriteCoordinates[4] = -6.825;
